ecc7d8a43d4dbfeb8501f6696e4618e7f2c001ae
Vahid Moradi <vahid.moradi@ibrows.ch>
2016-03-03 10:55:54 +0100

    Reducing the setup by simplifying it

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,12 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import play.mvc.*;
-
-import views.html.*;
 import views.html.index;
+
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok("Starting content");
-    }
-
-    public Result example() {
-        JsonNode json = request().body().asJson();
-        return ok(json.toString());
+        return ok(index.render("Starting Demo of JSON Parser"));
     }
 }
